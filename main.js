c4c2d7c0426eccb6e4be18bc10cfd78ffa00733d
Tea Mitchell <teamitchell@rocketmail.com>
2020-07-13 09:22:07 -0500

    added js class structure

---
+++ main.js
@@ -0,0 +1,31 @@
+class Traveler {
+    constructor (name) {
+        this.name = name
+        this.food = 1
+        this.isHealthy = true
+    }
+    hunt(){
+
+    }
+
+    eat(){
+
+    }
+}
+
+class Wagon{
+    constructor(capacity){
+        this.capacity = maximum
+        this.passengers = []
+    }
+    getAvailableSeatCount(){
+
+    }
+    join(Traveler){
+
+    }
+    totalfood(){
+        
+    }
+
+}
