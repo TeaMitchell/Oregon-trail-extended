183ca4da7e8ae760a74dc23ce572634eef9964ee
Tea Mitchell <teamitchell@rocketmail.com>
2020-07-14 18:35:01 -0500

    added to class Traveler and Wagon

--- main.js
+++ main.js
@@ -1,31 +1,58 @@
 class Traveler {
-    constructor (name) {
+    constructor (name, food, isHealthy) {
         this.name = name
         this.food = 1
         this.isHealthy = true
     }
     hunt(){
 
+        this.food += 2
     }
 
     eat(){
-
+        if(this.food < 1){
+           return this.isHealthy = false
+        }else{
+            this.food -=1
+        }
+        
     }
 }
 
 class Wagon{
-    constructor(capacity){
-        this.capacity = maximum
+    constructor(capacity, passengers){
+        this.capacity = capacity
         this.passengers = []
     }
     getAvailableSeatCount(){
-
+        let seatCount  =this.capacity - this.passengers.length
+        return seatCount
     }
     join(Traveler){
-
+        if(this.getAvailableSeatCount() > 0){
+            this.passengers.push(Traveler)
+        }
     }
     totalfood(){
-        
+
     }
 
 }
+
+let wagon = new Wagon(2)
+// Create three travelers
+let henrietta = new Traveler('Henrietta')
+let juan = new Traveler('Juan')
+let maude = new Traveler('Maude')
+console.log(`Wagon Seat Count?: ${ wagon.getAvailableSeatCount() } – EXPECTED: 2. The wagon starts with 2 seats. We haven't added travelers to the wagon yet.`)
+wagon.join(henrietta)
+console.log(`Wagon Seat Count?: ${ wagon.getAvailableSeatCount() } – EXPECTED: 1. Henrietta just joined.`)
+wagon.join(juan)
+wagon.join(maude)  // There is no room for her!
+console.log(`Wagon Seat Count?: ${ wagon.getAvailableSeatCount() } – EXPECTED: 0 – There is no room for Maude, but Juan was able to join.`)
+henrietta.hunt()   // Henrietta goes in search of food.
+juan.eat()         // Juan eats – as Juan does. 🤣
+juan.eat()         // Juan has run out of food!
+console.log(juan)
+console.log(`Wagon Should Quarantine?: ${ wagon.shouldQuarantine() } – EXPECTED: true. Juan has run out of food and become unhealthy!`)
+console.log(`Wagon's Total Food?: ${ wagon.totalFood() } – EXPECTED: 3.`)
